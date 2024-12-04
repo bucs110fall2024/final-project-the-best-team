d17ed5abf4557becd0dc66ac33b10f68d8636670
Maxwell <mzapesochny@binghamton.edu>
2024-12-04 01:32:40 -0500

    ujti6

--- src/player.py
+++ src/player.py
@@ -20,5 +20,5 @@ class Player:
     self.y += 2
   def turn(self):
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    radians = math.atan2((self.y-mouse_y),(self.x-mouse_x))
+    radians = math.atan2((self.y-mouse_y),(mouse_x-self.x))
     self.angle = (radians * 180)/(math.pi)
