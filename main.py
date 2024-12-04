79a482c24c4924c6cb597db988bc144712c2702d
Maxwell <mzapesochny@binghamton.edu>
2024-12-04 01:24:34 -0500

    gwjo

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 import pygame
-
+from src.player import Player
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption("Joe Mann the Bowman")
 icon = pygame.image.load("assets/game_icon.png")
 pygame.display.set_icon(icon)
+
+player = Player(300,300,45,"player_image.png")
+player_image = pygame.image.load("assets/player_image.png")
+
 while True:
-   x=1
+    player.turn()
+    rotated_player_image = pygame.transform.rotate(player_image,player.angle)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          exit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_w:
+            player.move_up()
+          elif event.key == pygame.K_a:
+            player.move_left()
+          elif event.key == pygame.K_s:
+            player.move_down()
+          elif event.key == pygame.K_d:
+            player.move_right()
+    screen.fill((0,128,0))
+
+
+      #2. detect collisions and update models
+    screen.blit(rotated_player_image,(player.x,player.y))
+      #3. Redraw next frame
+
+      #4. Display next frame
+    pygame.display.flip()
--- src/player.py
+++ src/player.py
@@ -3,10 +3,11 @@ import math
 class Player:
   
   def __init__(self,x,y,angle,image):
-    x.self = x
-    y.self = y
-    angle.self = angle
-    image.self = image
+    self.x = x
+    self.y = y
+    self.angle = angle
+    self.image = image
+    self.rotated_image = image
     #setup pygame data
     
   def move_left(self):
@@ -14,16 +15,10 @@ class Player:
   def move_right(self):
     self.x += 2
   def move_up(self):
-    self.y += 2
-  def move_down(self):
     self.y -= 2
+  def move_down(self):
+    self.y += 2
   def turn(self):
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    self.angle = math.atan(math.abs(self.y-mouse_y)/math.abs(self.x-mouse_x))*(180/math.pi())
-    if((mouse_y-self.y)<0 and (mouse_x-self.x)<0):
-      self.angle = self.angle + 180
-    elif((mouse_y-self.y)<0):
-      self.angle = self.angle + 270
-    elif((mouse_x-self.x)<0):
-      self.angle = self.angle + 90
-    
+    radians = math.atan2((self.y-mouse_y),(self.x-mouse_x))
+    self.angle = (radians * 180)/(math.pi)
